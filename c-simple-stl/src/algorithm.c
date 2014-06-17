a4b6fb15d0472a875c11b7d3bf12627ba0207e88
Jian <jianfan.tw@gmail.com>
2014-06-17 22:36:37 +0800

    Remove algorithm.h

--- c-simple-stl/src/algorithm.c
+++
@@ -1,7 +0,0 @@
-#include <algorithm.h>
-#include <stdlib.h>
-
-void sort(void **data, const unsigned int length, int (*compare)(const void *, const void *))
-{
-    qsort(data, length, sizeof(void *), compare);
-}
