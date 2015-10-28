e7477e9ca923a5bea407b4590bd3f5a7e78f9823
Yang-jian Fan <jianfan.tw@gmail.com>
2015-10-28 09:55:42 +0800

    Avoid redundant operations

--- src/ring_buffer.c
+++ src/ring_buffer.c
@@ -69,14 +69,17 @@ int ringbuffer_push(ringbuffer_t *ring_buffer, void *data, unsigned int data_siz
 {
     pthread_mutex_lock(&(ring_buffer->mutex));
 
-    data_size = MIN(data_size, ring_buffer->size - ring_buffer->in + ring_buffer->out);
+    const unsigned int available_size = ring_buffer->size - ring_buffer->in + ring_buffer->out;
 
-    const unsigned int length = MIN(data_size, ring_buffer->size - (ring_buffer->in & (ring_buffer->size - 1)));
+    data_size = MIN(data_size, available_size);
 
-    memcpy(ring_buffer->buffer + (ring_buffer->in & (ring_buffer->size - 1)), data, length);
+    const unsigned int in_offset = ring_buffer->in & (ring_buffer->size - 1);
+    const unsigned int copy_length = MIN(data_size, ring_buffer->size - in_offset);
 
-    if (data_size > length) {
-        memcpy(ring_buffer->buffer, (uint8_t *)data + length, data_size - length);
+    memcpy(ring_buffer->buffer + in_offset, data, copy_length);
+
+    if (data_size > copy_length) {
+        memcpy(ring_buffer->buffer, (uint8_t *)data + copy_length, data_size - copy_length);
     }
 
     ring_buffer->in += data_size;
@@ -90,14 +93,17 @@ int ringbuffer_pop(ringbuffer_t *ring_buffer, void *data, unsigned int data_size
 {
     pthread_mutex_lock(&(ring_buffer->mutex));
 
-    data_size = MIN(data_size, ring_buffer->in - ring_buffer->out);
+    const unsigned int used_size = ring_buffer->in - ring_buffer->out;
 
-    const unsigned int length = MIN(data_size, ring_buffer->size - (ring_buffer->out & (ring_buffer->size - 1)));
+    data_size = MIN(data_size, used_size);
 
-    memcpy(data, ring_buffer->buffer + (ring_buffer->out & (ring_buffer->size - 1)), length);
+    const unsigned int out_offset = ring_buffer->out & (ring_buffer->size - 1);
+    const unsigned int copy_length = MIN(data_size, ring_buffer->size - out_offset);
 
-    if (data_size > length) {
-        memcpy((uint8_t *)data + length, ring_buffer->buffer, data_size - length);
+    memcpy(data, ring_buffer->buffer + out_offset, copy_length);
+
+    if (data_size > copy_length) {
+        memcpy((uint8_t *)data + copy_length, ring_buffer->buffer, data_size - copy_length);
     }
 
     ring_buffer->out += data_size;
